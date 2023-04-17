82d0bc126a45b3fc0e1523283acbd609479e0d94
민동주 <ods1room@gmail.com>
2023-04-17 17:27:23 +0900

    Feat: Frontend 스켈레톤 코드 작성 S08P31A501-130

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <!-- Header -->
+    <router-view name="header" />
+    <!-- Main -->
+    <v-main>
+      <router-view />
+    </v-main>
+    <!-- Footer -->
+    <router-view name="footer" />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+@import "@/assets/styles/font.css";
+@import "@/assets/styles/variable.css";
+
+html body {
+  background: #fafafa;
+  max-width: 500px;
+  margin: 0 auto;
+  height: 100%;
+  min-height: 100%;
+}
+#app {
+  background-color: var(--main-col-1);
+  max-width: 100%;
+  min-height: 100%;
+
+  font-family: var(--main-font-1);
+}
+</style>
---
+++ frontend/src/views/Header/ListHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <v-btn @click="goBack">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      목록
+      <v-btn @click="deleteUser">
+        <v-icon>mdi-account-remove-outline</v-icon>
+      </v-btn>
+    </div>
+    <router-link to="chat">채팅</router-link> |
+    <router-link to="myEmoji">나의 이모지</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      if (this.$router.history.length > 1) {
+        this.$router.go(-1);
+      } else {
+        this.$router.push("/home");
+      }
+    },
+    deleteUser() {
+      alert("회원 탈퇴 버튼을 눌렀습니다.");
+    },
+  },
+};
+</script>
+
+<style></style>
