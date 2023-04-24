c6e9cd591aea9e62fb7b5001f8df920331f8f2bb
윤혜진 <hjyoon0223@gmail.com>
2023-04-24 17:10:48 +0900

    Feat: 빠진 파일 commit

--- springboot-framework/chat-service/src/main/java/com/wimoji/repository/dto/ChatRoomReq.java
+++ springboot-framework/chat-service/src/main/java/com/wimoji/repository/dto/ChatRoomReq.java
@@ -1,12 +1,9 @@
 package com.wimoji.repository.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class ChatRoomReq {
-	// TODO: emoji 연동
 	String emoji;
 	String name;
 }
