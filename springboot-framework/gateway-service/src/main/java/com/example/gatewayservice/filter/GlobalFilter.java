98db5a7c53eab4a2b2e1112498bfc347d2b1d3b1
손유진 <lyl0896@naver.com>
2023-04-26 11:16:09 +0900

    Feat: gateway-service filter 변경

--- springboot-framework/gateway-service/src/main/java/com/example/gatewayservice/filter/GlobalFilter.java
+++ springboot-framework/gateway-service/src/main/java/com/example/gatewayservice/filter/GlobalFilter.java
@@ -23,14 +23,13 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 			ServerHttpRequest request = exchange.getRequest();
 			ServerHttpResponse response = exchange.getResponse();
 
-			log.info("Global Filter baseMessage: {}",config.getBaseMessage());
-
 			if(config.isPreLogger()){
-				log.info("Global Filter Start: request id -> {}", request.getId());
+				log.info("request path : {}", request.getPath());
+				log.info("request query params : {}", request.getQueryParams());
 			}
 			return chain.filter(exchange).then(Mono.fromRunnable(()->{
 				if(config.isPostLogger()){
-					log.info("Global Filter End: response id -> {}", response.getStatusCode());
+					log.info("response status : {}", response.getStatusCode());
 				}
 			}));
 		};
@@ -39,7 +38,6 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 	@Data
 	public static class Config{
 		//Put the configuration properties
-		private String baseMessage;
 		private boolean preLogger;
 		private boolean postLogger;
 	}
