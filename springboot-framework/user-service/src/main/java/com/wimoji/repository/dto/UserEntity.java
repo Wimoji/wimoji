8ab8d8a9b6ca5f782591293b3360836811a3cdf4
민동주 <ods1room@gmail.com>
2023-04-26 17:50:50 +0900

    Fix: Annotation 추가

--- springboot-framework/user-service/src/main/java/com/wimoji/repository/dto/UserEntity.java
+++ springboot-framework/user-service/src/main/java/com/wimoji/repository/dto/UserEntity.java
@@ -1,7 +1,9 @@
 package com.wimoji.repository.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,6 +12,8 @@ import java.util.*;
 @Document(collection = "user")
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
     @Id
     String uid;
