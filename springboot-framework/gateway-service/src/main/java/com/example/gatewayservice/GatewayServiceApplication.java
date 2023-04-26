55278905ad042e8e0f063870cd6e29a30f5c0fdf
손유진 <lyl0896@naver.com>
2023-04-26 13:48:49 +0900

    Feat: eureka dependency 추가 및 어노테이션 순서 변경

--- springboot-framework/gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ springboot-framework/gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
