b2dd987faca6f9ba1b49b3203f7a4cf08ae6350d
손유진 <lyl0896@naver.com>
2023-04-26 13:26:30 +0900

    Feat: Logging 중복 처리되는 부분 제거 및 Discovery Service에 등록될 수 있도록 어노테이션 작성

--- springboot-framework/gateway-service/src/main/java/com/example/gatewayservice/filter/CustomFilter.java
+++ springboot-framework/gateway-service/src/main/java/com/example/gatewayservice/filter/CustomFilter.java
@@ -4,12 +4,10 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
-@Component
 @Slf4j
 public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
 	public CustomFilter(){
--- springboot-framework/gateway-service/src/main/java/com/example/gatewayservice/filter/LoggingFilter.java
+++ springboot-framework/gateway-service/src/main/java/com/example/gatewayservice/filter/LoggingFilter.java
@@ -6,13 +6,11 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
-@Component
 @Slf4j
 public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
 	public LoggingFilter(){
