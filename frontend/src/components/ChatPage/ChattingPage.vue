5ff1dad98744e2a76e3c644e34f7174cf74c27b5
윤혜진 <hjyoon0223@gmail.com>
2023-04-27 14:04:51 +0900

    Feat: 화면에 필요한 모든 함수 추가 S08P31A501-241

--- frontend/src/components/ChatPage/ChattingPage.vue
+++ frontend/src/components/ChatPage/ChattingPage.vue
@@ -6,13 +6,14 @@
       <p>방 이름: {{ room.name }}</p>
     </div>
     <div class="msg">
-      <div v-for="(message, index) in messages" :key="index">
+      <div v-for="message in messages" :key="message.id">
         <div>닉네임: {{ message.sender }}</div>
         <div>내용: {{ message.content }}</div>
       </div>
     </div>
     <div class="form">
-      <input v-model="sender" placeholder="닉네임란">
+      <input v-model="userName" placeholder="닉네임란(한번 적고 바꾸면 안됨)">
+      <input v-model="userId" placeholder="아이디란(안 겹칠만한 거 한번 적고 바꾸면 안됨)">
       <input v-model="content" placeholder="입력해주세요" @keyup.enter="sendMessage">
       <button @click="sendMessage">Send</button>
     </div>
@@ -22,6 +23,7 @@
 <script>
 import StompJS from "stompjs";
 import SockJS from "sockjs-client";
+import axios from 'axios';
 
 export default {
   data() {
@@ -29,62 +31,105 @@ export default {
       serverURL: 'http://70.12.246.229:8080',
       messages: [],
       room: [],
-      sender: '',
+      lastReadId: '',
+      userId: '',
+      userName: '',
       content: '',
       socket: null,
+      lastActiveTime: null,
     };
   },
   created() {
-    this.connect();
     this.room = this.$route.params.data;
-  },
-  mounted() {
-    this.getRoom();
+    console.log("이 방의 rid >>> ", this.room.rid);
+    console.log("이 방의 id >>> ", this.room.id);
+
+    this.lastActiveTime = Date.now();
+    setInterval(this.checkUserActivity, 300000);
+    
+    this.getLastReadId().then((lastReadId) => {
+      if(!lastReadId) {
+        this.connect();
+      } else {
+        this.getNewMessage(lastReadId);
+      }
+    });
   },
   methods: {
     goChat() {
       this.$router.push("/chat");
     },
-    getRoom() {
-      console.log("이 방의 rid >>> ", this.room.rid);
-      console.log("이 방의 id >>> ", this.room.id);
-    },
-    sendMessage() {
-      console.log("Send message:" + this.content);
-      if (this.socket && this.socket.connected) {
-        const msg = { 
-          rid: this.room.id,
-          sendTime: Date.now(),
-          sender: this.sender,
-          content: this.content,
-        };
-        this.socket.send("/pub/chat/message", { token: "" }, JSON.stringify(msg));
-      }
-      this.content = "";
+    getLastReadId() {
+      return new Promise((resolve) => {
+        axios.get("/read")
+        .then((response) => {
+          this.lastReadId = response.data.mid;
+          resolve(this.lastReadId);
+        }).catch(error => {
+          // db 오류
+          console.log(error);
+        });
+      });
     },
     connect() {
-      var sock = new SockJS(`${this.serverURL}/ws/chat`);
-      this.socket = StompJS.over(sock);
+      const sockJs = new SockJS(`${this.serverURL}/ws/chat`);
+      this.socket = StompJS.over(sockJs);
 
-      this.socket.connect(
-      {},
+      this.socket.connect({},
       frame => {
         console.log('소켓 연결 성공: ', frame);
-        this.socket.subscribe(`/sub/chat/${this.room.id}`,
-          msg => {
-            console.log("전달 메세지: ", msg);
-            this.messages.push(JSON.parse(msg.body));
-          }
-        );
-      },
-      error => {
+        this.socket.subscribe(`/sub/chat/${this.room.id}`, msg => {
+          // console.log("전달 메세지: ", msg);
+          this.messages.push(JSON.parse(msg.body));
+        });
+      }, error => {
         console.log("소켓 연결 실패", error);
+        // rid 오류
+        // 인원 최대
         if(error.command === "ERROR"){
           alert("채팅방의 인원이 최대입니다.")
           this.goChat()
         }
+      });
+    },
+    sendMessage() {
+      // console.log("Send message:" + this.content);
+      if (this.socket && this.socket.connected) {
+        const msg = { 
+          rid: this.room.id,
+          sender: this.userId,
+          content: this.content,
+        };
+        this.socket.send("/pub/chat/message", { token: "" }, JSON.stringify(msg));
+        // 형식 오류
+        // db 저장 오류
       }
-    );
+      this.content = "";
+      this.lastActiveTime = Date.now();
+    },
+    getNewMessage(id) {
+      axios.get(`/unread` + id)
+      .then((response) => {
+        const newMessages = response.data;
+        this.messages.push(...newMessages);
+      }).catch(error => {
+          // id 오류
+          // db 오류
+          console.log(error);
+      });
+    },
+    checkUserActivity() {
+      const timeDiff = Date.now() - this.lastActiveTime;
+      // 5분 동안 활동이 없으면 WebSocket disconnect
+      if (timeDiff > 300000) {
+        this.disconnectWebSocket();
+      }
+    },
+    disconnectWebSocket() {
+      this.socket.disconnect(() => {
+        console.log('WebSocket 연결 종료.');
+        this.goChat();
+      });
     },
   },
 };
