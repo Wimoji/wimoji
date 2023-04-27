a99bfa2360c32ced2d5b6d041d2d4370e1fe4587
민동주 <ods1room@gmail.com>
2023-04-27 09:46:31 +0900

    Fix: 로그인 로그아웃 파라미터 확인 수정 S08P31A501-186

--- springboot-framework/user-service/src/main/java/com/wimoji/repository/UserRepository.java
+++ springboot-framework/user-service/src/main/java/com/wimoji/repository/UserRepository.java
@@ -16,23 +16,33 @@ public class UserRepository {
     public void save(UserEntity userEntity) {
         mongoTemplate.save(userEntity);
     }
+
     public UserEntity findById(String uid) {
         return mongoTemplate.findById(uid, UserEntity.class);
     }
+
+    /**
+     * 로그인, 로그아웃
+     *
+     * @param id
+     * @param password
+     * @param isLogin
+     * @return
+     */
     public UserEntity findAndModify(String id, String password, boolean isLogin) {
         Query query = new Query();
         Criteria criteria = new Criteria();
 
-        if (password == null)
+        if (password != null) { //로그인 : 아이디, 패스워드
             query.addCriteria(criteria.andOperator(
                     Criteria.where("uid").is(id),
                     Criteria.where("password").is(password)
             ));
-        else
+        } else { //로그아웃 : 아이디
             query.addCriteria(criteria.andOperator(
                     Criteria.where("uid").is(id)
             ));
-
+        }
         //로그인 상태 변경
         UserEntity userEntity = mongoTemplate.findAndModify(
                 query,
@@ -41,6 +51,7 @@ public class UserRepository {
         );
         return userEntity;
     }
+
     public UserEntity findAndRemove(String id) {
 
         Query query = new Query();
