a4a424b9af51551f4bf149a1d02913ce64610024
함정빈 <shadberry02@naver.com>
2023-04-27 14:01:51 +0900

    Feat: add annotation

--- springboot-framework/emoji-service/src/main/java/com/wimoji/repository/Entity/Emoji.java
+++ springboot-framework/emoji-service/src/main/java/com/wimoji/repository/Entity/Emoji.java
@@ -1,10 +1,14 @@
 package com.wimoji.repository.Entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Emoji {
     String eid;
     String content;
