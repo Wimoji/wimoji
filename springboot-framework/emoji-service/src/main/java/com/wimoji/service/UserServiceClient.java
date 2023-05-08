2f353d1a6664f8e18e0515a30ccf871fdb1f67bc
hjle2 <02q09q@naver.com>
2023-05-08 12:37:13 +0900

    Feat: FeignClient방식으로 서버간 통신

---
+++ springboot-framework/emoji-service/src/main/java/com/wimoji/service/UserServiceClient.java
@@ -0,0 +1,13 @@
+package com.wimoji.service;
+
+import com.wimoji.base.dto.DataResponseDto;
+import com.wimoji.repository.Entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="user-service", url = "127.0.0.1:8080")
+public interface UserServiceClient {
+    @GetMapping("/api/user-service/userinfo")
+    String getUser(@RequestParam("bearerToken") String bearerToken);
+}
