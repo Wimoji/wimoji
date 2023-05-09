237f82a719523291df4f0089e0a72bf0d36ddd52
hjle2 <02q09q@naver.com>
2023-05-09 10:12:22 +0900

    Feat: Kafka 지우기

--- springboot-framework/user-service/src/main/java/com/wimoji/config/KafkaConfig.java
+++ springboot-framework/user-service/src/main/java/com/wimoji/config/KafkaConfig.java
@@ -17,40 +17,40 @@ import org.springframework.messaging.handler.annotation.SendTo;
 @Configuration
 @RequiredArgsConstructor
 public class KafkaConfig {
-    private final UserRepository userRepository;
-    private final JwtTokenUtil jwtTokenUtil;
-    private final ObjectMapper mapper;
-    @KafkaListener(id="server", topics = "userRequest")
-    @SendTo // use default replyTo expression
-    public String userRequest(String bearerToken) {
-
-        if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
-            return null;
-        }
-
-        String token = bearerToken.substring(7);
-        String uid = jwtTokenUtil.getUserIdFromToken(token);
-        String nickname = jwtTokenUtil.getUserNicknameFromToken(token);
-        UserEntity user = UserEntity.builder().uid(uid).nickname(nickname).build();
-
-        String userStr = null;
-        try {
-            userStr = mapper.writeValueAsString(user);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return userStr;
-    }
-
-    @Bean
-    public NewTopic kRequests() {
-        return new NewTopic("kRequests", 10, (short) 2);
-    }
-
-    @Bean // not required if Jackson is on the classpath
-    public MessagingMessageConverter simpleMapperConverter() {
-        MessagingMessageConverter messagingMessageConverter = new MessagingMessageConverter();
-        messagingMessageConverter.setHeaderMapper(new SimpleKafkaHeaderMapper());
-        return messagingMessageConverter;
-    }
+//    private final UserRepository userRepository;
+//    private final JwtTokenUtil jwtTokenUtil;
+//    private final ObjectMapper mapper;
+//    @KafkaListener(id="server", topics = "userRequest")
+//    @SendTo // use default replyTo expression
+//    public String userRequest(String bearerToken) {
+//
+//        if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
+//            return null;
+//        }
+//
+//        String token = bearerToken.substring(7);
+//        String uid = jwtTokenUtil.getUserIdFromToken(token);
+//        String nickname = jwtTokenUtil.getUserNicknameFromToken(token);
+//        UserEntity user = UserEntity.builder().uid(uid).nickname(nickname).build();
+//
+//        String userStr = null;
+//        try {
+//            userStr = mapper.writeValueAsString(user);
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
+//        return userStr;
+//    }
+//
+//    @Bean
+//    public NewTopic kRequests() {
+//        return new NewTopic("kRequests", 10, (short) 2);
+//    }
+//
+//    @Bean // not required if Jackson is on the classpath
+//    public MessagingMessageConverter simpleMapperConverter() {
+//        MessagingMessageConverter messagingMessageConverter = new MessagingMessageConverter();
+//        messagingMessageConverter.setHeaderMapper(new SimpleKafkaHeaderMapper());
+//        return messagingMessageConverter;
+//    }
 }
