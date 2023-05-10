4f297dbea0b2be5334ec16a648615bec68bccb2f
윤혜진 <hjyoon0223@gmail.com>
2023-05-10 15:56:10 +0900

    Refactor: 구조 단순화

--- springboot-framework/chat-service/src/main/java/com/wimoji/config/WebSocketConfig.java
+++ springboot-framework/chat-service/src/main/java/com/wimoji/config/WebSocketConfig.java
@@ -1,23 +1,14 @@
 package com.wimoji.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import com.wimoji.aop.SubscriptionInterceptor;
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-	private final SubscriptionInterceptor subscriptionInterceptor;
-
-	public WebSocketConfig(SubscriptionInterceptor subscriptionInterceptor) {
-		this.subscriptionInterceptor = subscriptionInterceptor;
-	}
-
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		// client가 webSocket handshake connection을 생성할 경로 설정
@@ -33,10 +24,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		registry.setApplicationDestinationPrefixes("/pub");
 		// 클라이언트가 메시지를 구독할 endpoint 정의
 		registry.enableSimpleBroker("/sub");
-	}
-
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration registration) {
-		registration.interceptors(subscriptionInterceptor);
 	}
 }
