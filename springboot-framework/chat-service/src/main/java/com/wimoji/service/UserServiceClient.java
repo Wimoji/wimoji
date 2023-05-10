f75796a197f3c6f5ffc3492cd6018f2d7d74b439
윤혜진 <hjyoon0223@gmail.com>
2023-05-10 15:28:27 +0900

    Refactor: 서버 주소로 변경

--- springboot-framework/chat-service/src/main/java/com/wimoji/service/UserServiceClient.java
+++ springboot-framework/chat-service/src/main/java/com/wimoji/service/UserServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="user-service", url = "127.0.0.1:8080")
+@FeignClient(name="user-service", url = "k8a501.p.ssafy.io:8080")
 public interface UserServiceClient {
     @GetMapping("/api/user-service/userinfo")
     String getUser(@RequestParam("bearerToken") String bearerToken);
