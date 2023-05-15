268e5f1c3b4740238b92398b8d690f69eb1a3f52
민동주 <ods1room@gmail.com>
2023-05-15 15:20:48 +0900

    Fix: axios 파라미터 수정

--- frontend/src/api/modules/location.js
+++ frontend/src/api/modules/location.js
@@ -4,10 +4,10 @@ const api = apiInstance();
 const baseURL = process.env.VUE_APP_KAKAOMAP_BASE_URL;
 
 // [GET] 현재 경도, 위도에서 주소 정보 가져오기
-async function getNowPosition(data, success, fail) {
+async function getNowPosition(params, success, fail) {
   await api
     .get(
-      `${baseURL}/v2/local/geo/coord2regioncode.json?x=${data.longitude}&y=${data.latitude}`
+      `${baseURL}/v2/local/geo/coord2regioncode.json?x=${params.longitude}&y=${params.latitude}`
     )
     .then(success)
     .catch(fail);
